be0475566eb667fca140cfac5fad39015d40bd2d
Akxary <pollxx97@gmail.com>
2025-03-19 02:17:01 +0300

    refcatoring

--- backend/recipes/authentication/authentication.py
+++ backend/recipes/authentication/authentication.py
@@ -1,4 +1,4 @@
-from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.authentication import JWTAuthentication, Token
 from rest_framework_simplejwt.exceptions import InvalidToken, AuthenticationFailed
 from django.contrib.auth import get_user_model
 
@@ -6,7 +6,7 @@ from django.contrib.auth import get_user_model
 Authors = get_user_model()
 
 class CustomJWTAuthentication(JWTAuthentication):
-    def get_user(self, validated_token):
+    def get_user(self, validated_token: Token):
         try:
             author_id = validated_token['author_id']
             user = Authors.objects.get(id=author_id)
